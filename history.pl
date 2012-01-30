3e0fb66fa2b54725b339c5aeb77f375e04977c02
Madis Janson <madis@cyber.ee>
2012-01-31 00:00:16 +0200

    history.

--- history.pl
+++ history.pl
@@ -3,7 +3,7 @@
 die "illegal arguments\n" unless $#ARGV == 1;
 ($fn, $key) = @ARGV;
 die "$fn: $!\n" unless open F, '<', $fn;
-die "illegal definition key" if $key =~ /[\x00-\x1f]/;
+die "illegal definition key\n" if $key =~ /[\x00-\x1f]/;
 $key = lc $key . "\t";
 $len = length $key;
 my $exists;
