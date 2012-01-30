1dbcfac30d912f187ac2cc2be83895758cd4b9d9
Madis Janson <madis@cyber.ee>
2012-01-30 23:54:46 +0200

    history in perl

---
+++ history.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+die "illegal arguments\n" unless $#ARGV == 1;
+($fn, $key) = @ARGV;
+die "$fn: $!\n" unless open F, '<', $fn;
+die "illegal definition key" if $key =~ /[\x00-\x1f]/;
+$key = lc $key . "\t";
+$len = length $key;
+my $exists;
+while (<F>) {
+	if ($key eq substr $_, 0, $len) {
+		split /\t/;
+		print $_[2] if $_[2] =~ /\S/;
+		$exists = 1
+	}
+}
+print "Mis $ARGV[1]?\n" unless $exists
