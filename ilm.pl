b03b1da5af7c348b782dcb3385eba9606cd41c3c
mth <mzz@localhost>
2021-06-26 03:26:24 +0300

    ilm

--- ilm.pl
+++ ilm.pl
@@ -1,12 +1,15 @@
 use Encode;
 
+#my $UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0';
+my $UA = 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0';
+
 sub http {
-	`curl -sf --connect-timeout '$_[0]' '$_[1]'`
+	`curl -sf -A '$UA' --connect-timeout '$_[0]' '$_[1]'`
 }
 
 $last_update = 0;
 #$emhi_url = 'http://213.184.50.180/ilma_andmed/xml/observations.php';
-$emhi_url = 'http://www.ilmateenistus.ee/ilma_andmed/xml/observations.php';
+$emhi_url = 'https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php';
 
 sub update {
 	print STDERR "ilmauuendus $dt\n";
@@ -20,7 +23,7 @@ sub update {
 			$name =~ s/ *\([^)]*\)//;
 		} elsif (/<airtemperature>(.*)<\/airtemperature>/) {
 			$temp{$name} = $1;
-			$temp{Tartu} = $temp{$name} if $name =~ /^Tartu-T/;
+			$temp{Tartu} = $temp{$name} if $name =~ /^Tartu-Kvissental/;
 			$temp{Tallinn} = $temp{$name} if $name eq 'Tallinn-Harku';
 			push @parnu, $temp{$name}
 				 if $name_ =~ /P.rnu/ && $temp{$name} =~ /\d/;
@@ -36,37 +39,41 @@ sub update {
 	if (/ Temperatuur +(-?\d+(\.\d+)?) /) {
 		my $tartu = $1;
 		my $tt = $temp{Tartu};
-		if (abs($tt - $tartu) < 4 or $tt !~ /\d/) {
+		#if (abs($tt - $tartu) < 4 or $tt !~ /\d/) {
+		if ($tt !~ /\d/) {
 			$temp{Tartu} = $tartu;
+#		} elsif (abs($tt - $tartu) < 4) {
 		} elsif (abs($tt - $tartu) <= 6) {
 			$temp{Tartu} = sprintf("%.1f", ($tt + $tartu) / 2);
+#			$temp{Tartu} = sprintf("%.1f", ($tt*2 + $tartu) / 3);
 		}
 	}
+	#uus url: http://tgftp.nws.noaa.gov/data/observations/metar/decoded/EETN.TXT
 	#@tallinn = `/usr/bin/lynx -source http://weather.noaa.gov/pub/data/observations/metar/stations/EETN.TXT`;
 	#if ($tallinn[1] =~ / (M?)(\d\d)\/M?\d\d /) {
 	#	$tallinn = $1 ? -$2 : $2;
 	#}
 	#@tallinn_ = split(/[ ']/, `/bin/nc 193.40.240.131 6340`);
 	#if (@tallinn_ > 2) {
-	my $tallinn = http(10, 'http://www.ilm.ee/~data/tallinn/temp');
-	chomp $tallinn;
-	if ($tallinn =~ /-?[\d.]+/ and
-			($tallinn ne '0.0' or $temp{Tallinn} !~ /\d/)) {
-		$tallinn = $&; # $tallinn_[2];
-		#$tallinn =~ tr/,/./;
-		my $tt = $temp{Tallinn};
-		if ($tt =~ /\d/) {
-			if (abs($tt - $tallinn) <= 6) {
-				$tallinn += $tt * 2;
-				$tallinn = sprintf("%.1f", $tallinn / 3);
-			} else { # valetab, raisk.
-				$tallinn = $tt;
-			}
-		}
-		$temp{Tallinn} = $tallinn;
-		#$tartu = http(10, 'http://www.ilm.ee/~data/tartu/temp');
-		#$temp{Tartu} = $tartu if $tartu =~ /\d/;
-	}
+#	my $tallinn = http(10, 'http://www.ilm.ee/~data/tallinn/temp');
+#	chomp $tallinn;
+#	if ($tallinn =~ /-?[\d.]+/ and
+#			($tallinn ne '0.0' or $temp{Tallinn} !~ /\d/)) {
+#		$tallinn = $&; # $tallinn_[2];
+#		#$tallinn =~ tr/,/./;
+#		my $tt = $temp{Tallinn};
+#		if ($tt =~ /\d/) {
+#			if (abs($tt - $tallinn) <= 6) {
+#				$tallinn += $tt * 2;
+#				$tallinn = sprintf("%.1f", $tallinn / 3);
+#			} else { # valetab, raisk.
+#				$tallinn = $tt;
+#			}
+#		}
+#		$temp{Tallinn} = $tallinn;
+#		#$tartu = http(10, 'http://www.ilm.ee/~data/tartu/temp');
+#		#$temp{Tartu} = $tartu if $tartu =~ /\d/;
+#	}
 	#($s,$m,$h) = localtime($last_update = time);
 	#$m = sprintf "%02d", $m;
 	#$ilm = "kell $h:$m";
@@ -77,6 +84,7 @@ sub update {
 	$ilm = join '; ', @ilm;
 	$ilm =~ s/([0-9])\.([0-9])/\1,\2/sg;
 	$ilm =~ s/\n//sg;
+	$last_update = time;
 }
 
 $| = 1;
