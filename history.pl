3eab80ea87676869d3cc00077690dc52fea7c8a6
mth <mzz@linux.ee>
2023-12-28 00:48:15 +0200

    compact history

--- history.pl
+++ history.pl
@@ -6,12 +6,13 @@ die "$fn: $!\n" unless open F, '<', $fn;
 die "illegal definition key\n" if $key =~ /[\x00-\x1f]/;
 $key = lc $key . "\t";
 $len = length $key;
-my $exists;
 while (<F>) {
 	if ($key eq lc (substr $_, 0, $len)) {
 		my @x = split /\t/;
-		print $x[2] if $x[2] =~ /\S/;
-		$exists = 1
+		next unless $x[2] =~ /\S++$/;
+		print $prev if $& ne $who;
+		$who = $&;
+		$prev = $x[2];
 	}
 }
-print "Mis $ARGV[1]?\n" unless $exists
+print $prev // "Mis $ARGV[1]?\n";
